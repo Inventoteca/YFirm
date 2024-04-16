dc87d8f5341a68f70631fd8e9c2dfce5434fa8b4
Invento-R7 <david.fc@inventoteca.com>
2024-04-15 22:05:52 -0600

    wifi wip

--- yumiso_1/wifiservice.cpp
+++ yumiso_1/wifiservice.cpp
@@ -26,14 +26,14 @@ bool wifiAP(bool force)
   wifiManager.setConfigPortalBlocking(false);
   // captive portal redirection
   //wifiManager.setCaptivePortalEnable(false);
-  wifiManager.setTimeout(300);
+  //wifiManager.setTimeout(300);
 
   //set config save notify callback
   wifiManager.setSaveParamsCallback(saveConfigCallback);
   wifiManager.setSaveConfigCallback(saveWifiCallback);
   wifiManager.setWebServerCallback(bindServerCallback);
   wifiManager.setBreakAfterConfig(true); // needed to use saveWifiCallback
-  wifiManager.setConfigPortalTimeout(300);
+  //wifiManager.setConfigPortalTimeout(300);
   //wifiManager.setParamsPage(true); // move params to seperate page, not wifi, do not combine with setmenu!
 
   //set static ip
